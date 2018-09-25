e89fc631a8a7e2682326433381d6da0818264ff6
Isak Lindhé <isak.e.lindhe@gmail.com>
2018-09-25 18:36:29 +0200

    cleaned up old code

--- connect4.py
+++ connect4.py
@@ -1,50 +1,65 @@
-def checkwin(board):
-    bricks = [(x,y) for x in range(len(board)) for y in range(len(board[x]))]
-    for i in bricks:
-        for j in [(1,0),(0,1),(1,1),(-1,1)]:
-            if checkline(i[0],i[1],j[0],j[1],board[i[0]][i[1]]) >=4:
-                return board[i[0]][i[1]]
-    return 0
+class Connect4Game:
+    def __init__(self, height=7, width=7, n_players=2):
+        self.height = height
+        self.width = width
+        self.n_players = n_players
+        self.board = self.empty_board()
 
-def checkline(x,y,dx,dy,p):
-    if 0<=x<len(board) and 0<=y<len(board[x]) and board[x][y] == p:
-        return 1 + checkline(x+dx,y+dy,dx,dy,p)
-    else:
-        return 0
+    def checkwin(self):
+        board = self.board
+        bricks = [(x,y) for x in range(len(board)) for y in range(len(board[x]))]
+        for i in bricks:
+            for j in [(1,0),(0,1),(1,1),(-1,1)]:
+                if self.checkline(i[0],i[1],j[0],j[1],board[i[0]][i[1]]) >=4:
+                    return board[i[0]][i[1]]
+        return None
 
-def printboard(w,h,board):
-    out = ""
-    for y in range(h):
-        for x in range(w-1,-1,-1):
-            if x>=len(board) or y>=len(board[x]):
-                out = ". " + out
-            else:
-                out = str(board[x][y]) + " " + out
-        out = "\n" + out
-    out += "\n"+"--"*w + "\n"
-    for i in range(1,w+1):
-        out+=str(i)+" "
-    print(out)
+    def checkline(self, x, y, dx, dy, p):
+        board = self.board
+        if 0<=x<len(board) and 0<=y<len(board[x]) and board[x][y] == p:
+            return 1 + self.checkline(x+dx,y+dy,dx,dy,p)
+        else:
+            return 0
 
-def move(p,col,w,h,board):
-    if 0<=col-1<w and len(board[col-1])<h:
-        board[col-1].append(p)
+    def print_board(self):
+        board = self.board
+        w = self.width
+        h = self.height
+        out = ""
+        for y in range(h):
+            for x in range(w-1,-1,-1):
+                if x>=len(board) or y>=len(board[x]):
+                    out = ". " + out
+                else:
+                    out = str(board[x][y]) + " " + out
+            out = "\n" + out
+        out += "\n"+"--"*w + "\n"
+        for i in range(1,w+1):
+            out+=str(i)+" "
+        print(out)
 
-board = [[],[],[],[],[],[],[]]
-players = 2
-height = 7
-width = 7
+    def move(self, p, col):
+        board = self.board
+        w = self.width
+        h = self.height
+        if 0<=col-1<w and len(board[col-1])<h:
+            board[col-1].append(p)
 
-printboard(width,height,board)
-win = 0
-while win == 0:
-    for i in range(1,players+1):
-        prompt = ""
-        while prompt == "":
-            prompt = input("player"+str(i)+"> ")
-        move(i,int(prompt),width,height,board)
-        printboard(width,height,board)
-        win = checkwin(board)
-        if win!=0:
-            print("player "+str(i)+" wins!")
-            break
+    def empty_board(self):
+        return [[] for i in range(self.width)]
+
+if __name__ == '__main__':
+    game = Connect4Game()
+    game.print_board()
+    win = None
+    while win == None:
+        for i in range(1,game.n_players+1):
+            prompt = ""
+            while prompt == "":
+                prompt = input("player"+str(i)+"> ")
+            game.move(i, int(prompt))
+            game.print_board()
+            win = game.checkwin()
+            if win != None:
+                print("player "+str(i)+" wins!")
+                break
